c33e2d2479d82bbb78cc84af8df58caeb53e1cc8
luis2003 <luis2003@gmail.com>
2021-06-25 20:25:37 -0300

    docs: adding clientid info in setup.sh

--- auth/auth.py
+++ auth/auth.py
@@ -7,7 +7,7 @@ import logging
 import os
 
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-ALGORITHMS = os.environ.get['ALGORITHMS']
+ALGORITHMS = os.environ.get('ALGORITHMS')
 API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 
